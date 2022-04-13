35c611de087e9b174f831a287db10e0e2b036b66
PhucLuan <phucluan6052000@gmail.com>
2022-04-14 00:52:50 +0700

    update AuthenticationGuard

--- src/app/core/authentication/authentication.guard.ts
+++ src/app/core/authentication/authentication.guard.ts
@@ -2,16 +2,30 @@ import { Injectable } from '@angular/core';
 import {
   CanActivate,
   ActivatedRouteSnapshot,
-  RouterStateSnapshot,
+  RouterStateSnapshot,Router
 } from '@angular/router';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private oidcSecurityService: OidcSecurityService
+  ) {}
+
+  public isAuthenticated: boolean;
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    this.oidcSecurityService
+      .checkAuth()
+      .subscribe((auth) => (this.isAuthenticated = auth));
+
+    if (this.isAuthenticated) return true;
+    this.router.navigate(['/']);
+    return false;
   }
 }
